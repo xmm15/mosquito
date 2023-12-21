cb25d90897c6ffe532e9481879a053f4761057d5
vic <murimimlvictor@gmail.com>
2023-12-21 14:59:20 +0300

    stream

---
+++ src/stream.c
@@ -0,0 +1,60 @@
+#include "stream.h"
+
+size_t stream_read(stream_t *strm, char *buff, size_t *n){
+    size_t rb = 0;
+    int total = 0;
+
+    while(1){
+        rb = read(strm->handler, buff,n);
+
+        total += rb;
+
+        if(rb == 0)
+            break;
+        
+        if(rb < 0){
+            total = -1;
+            break;
+        }
+        
+
+        if(total >= n) break;
+    }
+
+    return total;
+}
+
+size_t stream_write(stream_t *strm, char *buff, size_t *n){
+    size_t wb = 0;
+    int total = 0;
+
+    while(1){
+        wb = write(strm->handler, buff, n);
+
+        total += wb;
+
+        if(wb == 0)
+            break;
+        
+        if(wb < 0){
+            total = -1;
+            break;
+        }
+        
+
+        if(total >= n) break;
+    }
+
+    return total;
+}
+
+void stream_close(stream_t *strm){
+    close(strm->handler);
+}
+
+stream_t *stream_create(){
+    stream_t *tmp = malloc(sizeof(stream_t));
+    tmp->handler = -1;
+
+    return tmp;
+}
---
+++ src/stream.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct stream stream_t;
+
+struct stream {
+    int handler;
+};
+
+size_t stream_read(stream_t *strm, char *buff, size_t *n);
+
+size_t stream_write(stream_t *strm, char *buff, size_t *n);
+
+void stream_close(stream_t *strm);
+
+stream_t *stream_create();
